732b20fe1167d0374f4a5c0112dcf5a10330ac12
Ivan Panchev <ikp516@student.bham.ac.uk>
2017-02-03 15:30:21 +0000

    Imports in the Client added

--- src/ClientNetworking/Client.java
+++ src/ClientNetworking/Client.java
@@ -10,6 +10,12 @@
 // the server dies.
 package ClientNetworking;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 // TODO: Auto-generated Javadoc
 /**
  * The Class Client.
