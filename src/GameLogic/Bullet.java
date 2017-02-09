2f5a67cb27a6fff3bf549b56a32d20f27beb5f71
Jivan Pal <jivan.pal@gmail.com>
2017-02-09 12:34:57 +0000

    Fixed constructor call to super() in GameLogic.Bullet

--- src/GameLogic/Bullet.java
+++ src/GameLogic/Bullet.java
@@ -1,19 +1,28 @@
 package GameLogic;
 
-import Geometry.Rotation;
-import Geometry.Vector;
-import Physics.Body;
+import Geometry.*;
+import Physics.*;
 
 public class Bullet extends Body {
 	
 	private double damageToShip;
 	private double damageToShield; 
 	
-	public Bullet (double mass, Vector position, Rotation orientation,
-	        Vector velocity, Vector angularVelocity, int damageToShip, int damageToShield) {
-			super(mass,position,orientation,velocity,angularVelocity);
-			this.damageToShield = damageToShield;
-			this.damageToShip = damageToShip;
-	        
-	    }
+	public Bullet (
+        int      damageToShip,
+        int      damageToShield,
+        double   mass,
+        double   radius,
+        Vector   position,
+        Rotation orientation,
+        Vector   velocity,
+        Vector   angularVelocity
+    ) {
+        super(mass, radius, position, orientation, velocity, angularVelocity);
+        
+        this.damageToShield = damageToShield;
+        this.damageToShip = damageToShip;
+    }
+	
+	
 }
