dc6c24973563590da6190d3e8328852e87dd7a2d
Jivan Pal <jivan.pal@gmail.com>
2017-02-09 20:26:28 +0000

    Reimplemented GameLogic.Resource; resource-specific change values are unnecessary; assumed to be 1 in all contexts instead.

--- src/GameLogic/Resource.java
+++ src/GameLogic/Resource.java
@@ -1,43 +1,104 @@
 package GameLogic;
 
+/**
+ * A simple container for a bounded integer.
+ * Used to represent weapon levels and capacity.
+ * @author jivan
+ *
+ */
 public class Resource {
+/// FIELDS
     private int level;
-    private final int MAX_LEVEL;
-    private final int CHANGE;
+    private int max;
     
-    public Resource (int l, int max, int change) {
-        level = l;
-        MAX_LEVEL = max;
-        CHANGE = change;
+/// CONSTRUCTORS
+    
+    /**
+     * Create a resource with given maximum and initial levels.
+     */
+    public Resource (int max, int level) {
+        setMax(max);
+        set(level);
     }
     
-    public void change(int delta) {
-        level += delta;
-        if (level < 0) {
-            level = 0;
-        } else if (level > MAX_LEVEL) {
-            level = MAX_LEVEL;
-        }
+    /**
+     * Create a resource with given maximum level, with initial level set to 0.
+     */
+    public Resource (int max) {
+        this(max, 0);
     }
     
-    public void increase() {
-        this.change(CHANGE);
-    }
+/// INSTANCE METHODS
     
-    public void decrease() {
-        this.change(-CHANGE);
-    }
+// Getters
     
+    /**
+     * Get the current level of this instance.
+     */
     public int get() {
         return level;
     }
     
-    public int getChange(){
-    	return CHANGE;
+    /**
+     * Get the maximum level of this instance.
+     */
+    public int getMax() {
+        return max;
     }
     
-    public int getMaxLevel(){
-    	return MAX_LEVEL;
+    /**
+     * Get a number representing how full this instance is.
+     * @return  a double between 0.0 and 1.0 (inclusive) representing how full this instance is.
+     */
+    public double getFraction() {
+        return (double)level/max;
     }
- 
+    
+// Setters
+    
+    /**
+     * Set the level of this instance.
+     * @param   level   The level to set this instance to.
+     *              If it is below 0, the level will be set to 0.
+     *              If it is above the current maximum level,
+     *              it will be set to the maximum level.
+     */
+    public void set(int level) {
+        this.level =
+            level < 0   ?   0:
+            level > max ?   max:
+                            level;
+    }
+    
+    /**
+     * Change the level of this instance by the given amount.
+     * @param   delta   The amount to change this instance's level by.
+     *              If positive, it is increased.
+     *              If negative, it is decreased.
+     */
+    public void change(int delta) {
+        set(level + delta);
+    }
+    
+    /**
+     * Increase this instance's level by 1.
+     */
+    public void up() {
+        this.change(1);
+    }
+    
+    /**
+     * Decrease this instance's level by 1.
+     */
+    public void down() {
+        this.change(-1);
+    }
+    
+    /**
+     * Set/change the maximum level of this instance.
+     * @param   max The new maximum level.
+     */
+    public void setMax(int max) {
+        this.max = max;
+    }
 }
