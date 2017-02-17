3dfd9910c6ea98a248d4097eab57d789e7d31513
dst512 <dst512@cs.bham.ac.uk>
2017-02-17 14:16:52 +0000

    Did some very important stuff

--- src/ClientNetworking/GameHost/GameHostReceiver.java
+++ src/ClientNetworking/GameHost/GameHostReceiver.java
@@ -13,20 +13,48 @@ public class GameHostReceiver extends Thread
 {
 	private ObjectInputStream clientIN;
 	private Map gameMap=null;
-	private BufferedReader in;	
-	public GameHostReceiver(ObjectInputStream reader, Map gM)
+	private BufferedReader in;
+	private int playerPos;
+	public GameHostReceiver(ObjectInputStream reader, Map gM, int playerPos)
 	{		
 		in = new BufferedReader(new InputStreamReader(reader));
 		gameMap = gM;
+		this.playerPos = playerPos;
 	}
 
 	public void run()
 	{
-		while (true)
+		boolean running = true;
+		while (running)
 		{
 			try
 			{
 				String str = in.readLine();
+				switch(str){
+					case "fireWeapon1":
+						
+						break;
+					case "fireWeapon2":
+						break;
+					case "fireWeapon3":
+						break;
+					case "accelerate":
+						break;
+					case "decelerate":
+						break;
+					case "pitchDown":
+						break;
+					case "pitchUp":
+						break;
+					case "rollLeft":
+						break;
+					case "rollRight":
+						break;
+					case "exit":
+						running = false;
+					default:
+						throw new IllegalArgumentException("You done sent the wrong string yo");
+				}
 					
 			}
 
@@ -36,5 +64,6 @@ public class GameHostReceiver extends Thread
 			}
 
 		}
+		
 	}
 }
