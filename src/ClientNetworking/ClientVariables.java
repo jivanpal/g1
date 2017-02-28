ae927ff5967c91c89601c9a3cd2bac6f46595f3a
Jivan Pal <jivan.pal@gmail.com>
2017-02-28 14:40:31 +0000

    Added velocity change debugging messages; changed ship engine forces to fix unbalanced rolling; turned off collisions for testing purposes

--- src/ClientNetworking/ClientVariables.java
+++ src/ClientNetworking/ClientVariables.java
@@ -4,5 +4,5 @@ public class ClientVariables
 {
 	public static final int PORT = 8765;
 	public static final String HOSTNAME ="52.56.173.162";
-	//public static final String HOSTNAME = "localhost";
+//	public static final String HOSTNAME = "localhost";
 }
