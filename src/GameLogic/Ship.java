4466dbea686e55b18e34dc19456a45ab38bedb87
Jivan Pal <jivan.pal@gmail.com>
2017-03-10 13:27:16 +0000

    Added smooth-transition constants to Ship, but not yet implemented

--- src/GameLogic/Ship.java
+++ src/GameLogic/Ship.java
@@ -6,47 +6,56 @@ import Physics.*;
 /**
  * Class which represents the ship object in the game
  * @author Ivan Panchev
- *
+ * @author jivan
  */
 public class Ship extends Body{
-	private static final double PITCH_SPEED  = 0.05;    // radians per second
-    private static final double ROLL_SPEED   = 0.1;    // radians per second
-    private static final double THRUST_SPEED = 5;    // meters  per second
-	
-	public static final byte LASER_BLASTER_INDEX = 0;
-	public static final byte PLASMA_BLASTER_INDEX = 1;
-	public static final byte TORPEDO_WEAPON_INDEX = 2;
-
-	private String engineerName;
-	private String pilotName;
-	private Weapon torpedoWeapon;
-	private Weapon laserBlaster;
-	private Weapon plasmaBlaster;
-	private Engines engines;
-	private Shields shields;
-	private ShipHealth shipHealth;
-	
-	/**
-	 * Creates a new ship with a specific pilotName
-	 * @param pilotName The pilot name of the specific ship
-	 */
-	public Ship(String pilotName, String engineerName){
-		// Set mass and radius
-		super(100, 2);
-		
-		//Initialising weapons
-		torpedoWeapon = new TorpedoWeapon();
-		laserBlaster = new LaserBlaster();
-		plasmaBlaster = new PlasmaBlaster();
-		
-		//Initialising other parts
-		engines = new Engines();
-		shields = new Shields();
-		shipHealth = new ShipHealth();
-		
-		//assigning the pilot name to the ship as a way of identifying the ship 
-		this.pilotName = pilotName;
-		this.engineerName = engineerName;
+    // Maximum values for pitch velocity, roll velocity, and forward velocity.
+    // The ship is designed under the assumption that it cannot reverse,
+    //      therefore the reverse-velocity maximum is assumed to be zero.
+    private static final double PITCH_VEL_MAX = 0.1;  // radians per second
+    private static final double ROLL_VEL_MAX  = 0.3;  // radians per second
+    private static final double FWD_VEL_MAX   = 20;   // meters per second
+    
+    // Smoothness of transition from rest to maximum for each axis
+    // Higher values result in smoother transitions
+    private static final int    PITCH_SMOOTHNESS    = 10;
+    private static final int    ROLL_SMOOTHNESS     = 10;
+    private static final int    THRUST_SMOOTHNESS   = 4;
+    
+    public static final byte LASER_BLASTER_INDEX = 0;
+    public static final byte PLASMA_BLASTER_INDEX = 1;
+    public static final byte TORPEDO_WEAPON_INDEX = 2;
+    
+    private String engineerName;
+    private String pilotName;
+    private Weapon torpedoWeapon;
+    private Weapon laserBlaster;
+    private Weapon plasmaBlaster;
+    private Engines engines;
+    private Shields shields;
+    private ShipHealth shipHealth;
+    
+    /**
+     * Creates a new ship with a specific pilotName
+     * @param pilotName The pilot name of the specific ship
+     */
+    public Ship(String pilotName, String engineerName){
+        // Set mass and radius
+        super(100, 2);
+        
+        //Initialising weapons
+        torpedoWeapon = new TorpedoWeapon();
+        laserBlaster = new LaserBlaster();
+        plasmaBlaster = new PlasmaBlaster();
+        
+        //Initialising other parts
+        engines = new Engines();
+        shields = new Shields();
+        shipHealth = new ShipHealth();
+        
+        //assigning the pilot name to the ship as a way of identifying the ship 
+        this.pilotName = pilotName;
+        this.engineerName = engineerName;
 	}	
 	
 	public void updateWeaponsCooldown(){
@@ -143,41 +152,45 @@ public class Ship extends Body{
 		} else if(index == PLASMA_BLASTER_INDEX){
 			return plasmaBlaster.getMaxAmmo();
 		}
-
+        
         // Should never reach this
-		return -1;
-	}
-	
+        return -1;
+    }
+    
 // Movement methods
-	
-	private static final Rotation  PITCH_UP    = new Rotation(Vector.I.scale(PITCH_SPEED * Global.REFRESH_PERIOD));
-	private static final Rotation  PITCH_DOWN  = new Rotation(Vector.I.negate().scale(PITCH_SPEED * Global.REFRESH_PERIOD));
-	private static final Rotation  ROLL_RIGHT  = new Rotation(Vector.J.scale(ROLL_SPEED * Global.REFRESH_PERIOD));
-	private static final Rotation  ROLL_LEFT   = new Rotation(Vector.J.negate().scale(ROLL_SPEED * Global.REFRESH_PERIOD));
-	private static final Vector    THRUST_FWD  = Vector.J.scale(THRUST_SPEED);
-	private static final Vector    THRUST_REV  = Vector.J.negate().scale(THRUST_SPEED);
-	
-	public void pitchUp() {
-	    rotate(PITCH_UP);
-	}
-	
-	public void pitchDown() {
-	    rotate(PITCH_DOWN);
-	}
-	
-	public void rollRight() {
+    
+    private static final double PITCH_ACC   = PITCH_VEL_MAX / PITCH_SMOOTHNESS;
+    private static final double ROLL_ACC    = ROLL_VEL_MAX  / ROLL_SMOOTHNESS;
+    private static final double THRUST_ACC  = FWD_VEL_MAX   / THRUST_SMOOTHNESS;
+    
+    private static final Rotation   PITCH_UP    = new Rotation(Vector.I.scale(PITCH_ACC * Global.REFRESH_PERIOD));
+    private static final Rotation   PITCH_DOWN  = new Rotation(Vector.I.negate().scale(PITCH_ACC * Global.REFRESH_PERIOD));
+    private static final Rotation   ROLL_RIGHT  = new Rotation(Vector.J.scale(ROLL_ACC * Global.REFRESH_PERIOD));
+    private static final Rotation   ROLL_LEFT   = new Rotation(Vector.J.negate().scale(ROLL_ACC * Global.REFRESH_PERIOD));
+    private static final Vector     THRUST_FWD  = Vector.J.scale(THRUST_ACC);
+    private static final Vector     THRUST_REV  = Vector.J.negate().scale(THRUST_ACC);
+    
+    public void pitchUp() {
+        rotate(PITCH_UP);
+    }
+    
+    public void pitchDown() {
+        rotate(PITCH_DOWN);
+    }
+    
+    public void rollRight() {
         rotate(ROLL_RIGHT);
     }
-	
-	public void rollLeft() {
-	    rotate(ROLL_LEFT);
-	}
-	
-	public void thrustForward() {
-	    alterVelocityLocally(THRUST_FWD);
-	}
-	
-	public void thrustReverse() {
-	    alterVelocityLocally(THRUST_REV);
-	}
+    
+    public void rollLeft() {
+        rotate(ROLL_LEFT);
+    }
+    
+    public void thrustForward() {
+        alterVelocityLocally(THRUST_FWD);
+    }
+    
+    public void thrustReverse() {
+        alterVelocityLocally(THRUST_REV);
+    }
 }
