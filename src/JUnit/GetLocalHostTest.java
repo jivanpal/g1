d4123cdadcc9f9ff15dddf05325b61779dbbcee7
sxs1307 <sxs1307@student.bham.c.uk>
2017-03-17 12:50:10 +0000

    displaying all the ip's

--- src/JUnit/GetLocalHostTest.java
+++ src/JUnit/GetLocalHostTest.java
@@ -1,15 +1,31 @@
 package JUnit;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
-public class GetLocalHostTest {
-	public static void main(String[] args) {
-		try {
-			System.out.println(InetAddress.getLocalHost());
-		} catch (UnknownHostException e) {
+public class GetLocalHostTest
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			Enumeration<NetworkInterface> s = NetworkInterface.getNetworkInterfaces();
+			while (s.hasMoreElements())
+			{
+				Enumeration<InetAddress> x = s.nextElement().getInetAddresses();
+				while(x.hasMoreElements())
+				{
+					System.out.println(x.nextElement());
+				}
+			}
+		} catch (SocketException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 }
