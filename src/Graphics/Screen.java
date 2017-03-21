c723b4de664e92043d3b088005fac80f5edebaeb
dst512 <dst512@cs.bham.ac.uk>
2017-03-21 13:56:19 +0000

    Moved crosshair to correct position

--- src/Graphics/Screen.java
+++ src/Graphics/Screen.java
@@ -1,19 +1,15 @@
 package Graphics;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import javax.swing.JPanel;
 
 import GameLogic.Asteroid;
 import GameLogic.Bullet;
-import GameLogic.Global;
 import GameLogic.Map;
 import GameLogic.Ship;
 import Physics.Body;
-import jdk.management.resource.ResourceAccuracy;
 import Geometry.Vector;
 import GameLogic.Resource;
 
@@ -35,7 +31,6 @@ public class Screen extends JPanel{
 	
 	public static int nPoly = 0, nPoly3D = 0;
 	public static ArrayList<Poly3D> poly3Ds = new ArrayList<Poly3D>();
-	int drawOrder[];
 	boolean w, a, s, d, e, q;
 	
 	private Map map;
@@ -107,18 +102,9 @@ public class Screen extends JPanel{
 		
 		//Perform camera calculations based on current keypresses
 		camera();
-//		Calculations.setInfo();
 		setLight();
 		
 		g.setColor(Color.WHITE);
-
-//		for(Body b : starMap.bodies()){
-//			Star s = (Star) b;
-//			Vector v = s.getPosition();
-//			Point p = Calculations.calcPos(viewFrom, viewTo, v);
-//			g.fillOval((int)p.x, (int)p.y, 1, 1);
-//			g.drawOval((int)p.x, (int)p.y, 1, 1);
-//		}
 //
 		starMap.bodies().parallelStream()
 				.map(Star.class::cast)
@@ -133,19 +119,10 @@ public class Screen extends JPanel{
 		
 		//Draw all polygons onto the screen
 		nPoly = poly3Ds.size();
-		
-		for(int i = 0; i < nPoly; i++){
-			poly3Ds.get(i).update();
-		}
 
 		poly3Ds.parallelStream().forEach(poly3D -> poly3D.update());
 
 		setDrawOrder();
-		
-//		for(int i = 0; i < nPoly; i++){
-////			System.out.println("Drawing Polygon " + i);
-//			poly3Ds.get(drawOrder[i]).poly.drawPoly(g);
-//		}
 		
 		for(Poly3D p : poly3Ds){
 			p.poly.drawPoly(g);
@@ -160,8 +137,8 @@ public class Screen extends JPanel{
 		g.setColor(Color.WHITE);
 		
 		if(crosshair){
-			g.drawLine((int)getWidth()/2 - 5, (int)getHeight()/2, (int)getWidth()/2 + 5, (int)getHeight()/2);
-			g.drawLine((int)getWidth()/2, (int)getHeight()/2 - 5, (int)getWidth()/2, (int)getHeight()/2 + 5);
+			g.drawLine(GameLogic.Global.SCREEN_WIDTH/2 - 5, GameLogic.Global.SCREEN_HEIGHT/2, GameLogic.Global.SCREEN_WIDTH/2 + 5, GameLogic.Global.SCREEN_HEIGHT/2);
+			g.drawLine(GameLogic.Global.SCREEN_WIDTH/2, GameLogic.Global.SCREEN_HEIGHT/2 - 5, GameLogic.Global.SCREEN_WIDTH/2, GameLogic.Global.SCREEN_HEIGHT/2 + 5);
 		}
 		if(debug){
 	        g.drawString("viewFrom: "+viewFrom, 40, 40);
@@ -171,19 +148,19 @@ public class Screen extends JPanel{
 	        g.drawString("U: "+U,               40, 120);
 	        g.drawString("N: "+N,               40, 140);
         
-//	        for(Body b : map.bodies()) {
-//	            if (!(shipIndex == null) && b.getID() != shipIndex) {
-//	            	Class bClass = b.getClass();
-//	                if(bClass == Asteroid.class){
-//	                    for(Vector v : map.getAllPositions(b.getPosition())){
-//	                    	Point newPos = Calculations.calcPos(viewFrom, viewTo, v);
-//	                    	if(newPos.z > 0){
-//	                    		g.drawString("" + b.getID(), (int)(newPos.x + Global.SCREEN_WIDTH/2), (int)(newPos.y + Global.SCREEN_HEIGHT/2));
-//	                    	}
-//	                    }
-//	                }
-//	            }
-//	        }
+	        for(Body b : map.bodies()) {
+	            if (!(shipIndex == null) && b.getID() != shipIndex) {
+	            	Class bClass = b.getClass();
+	                if(bClass == Asteroid.class){
+	                    for(Vector v : map.getAllPositions(b.getPosition())){
+	                    	Point newPos = Calculations.calcPos(viewFrom, viewTo, v);
+	                    	if(newPos.z > 0){
+	                    		g.drawString("" + b.getID(), (int)(newPos.x + GameLogic.Global.SCREEN_WIDTH/2), (int)(newPos.y + GameLogic.Global.SCREEN_HEIGHT/2));
+	                    	}
+	                    }
+	                }
+	            }
+	        }
 		}
         
 		sleepAndRefresh();
@@ -383,37 +360,5 @@ public class Screen extends JPanel{
 				}
 			}
 		}
-	}
-
-	public void quicksort(double[] numbers, int low, int high)
-	{
-		 int i = low, j = high;
-         double pivot = numbers[low + (high-low)/2];
-
-         while (i <= j) {
-
-                 while (numbers[i] > pivot) {
-                         i++;
-                 }
-
-                 while (numbers[j] < pivot) {
-                         j--;
-                 }
-
-                 if (i <= j) {
-                	 	double temp = numbers[i];
-                	 	numbers[i] = numbers[j];
-                	 	numbers[j] = temp;
-                	 	int temp2 = drawOrder[i];
-                	 	drawOrder[i]=drawOrder[j];
-                	 	drawOrder[j]=temp2;
-                         i++;
-                         j--;
-                 }
-         }
-         if (low < j)
-                 quicksort(numbers,low, j);
-         if (i < high)
-                 quicksort(numbers,i, high);
 	}
 }
