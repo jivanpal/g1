454949e3858b5941da7c1a14527ccd63cb3da108
Svetlin <svetlioserafimov@gmail.com>
2017-03-23 11:31:46 +0000

    GAH A MISTAKE

--- src/ClientNetworking/ClientVariables.java
+++ src/ClientNetworking/ClientVariables.java
@@ -5,6 +5,6 @@ package ClientNetworking;
  */
 public class ClientVariables {
 	public static final int PORT = 8765;
-	//public static final String HOSTNAME ="52.56.181.11";
-	public static final String HOSTNAME = "localhost";
+	public static final String HOSTNAME ="52.56.181.11";
+	//public static final String HOSTNAME = "localhost";
 }
